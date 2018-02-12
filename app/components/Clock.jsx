471b35bf9f8fc040ca55ee98a2ae5e975d9224b9
Mark Saiget <saiget@gmail.com>
2018-02-11 20:20:45 -0800

    Add functions for generating hour and minute hands.

--- app/components/Clock.jsx
+++ app/components/Clock.jsx
@@ -4,32 +4,85 @@ import styled, {keyframes} from 'styled-components';
 class Clock extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            hourAnimating: false,
+            minuteAnimating: false,
+            hour: 0,
+            minute: 0
+        };
+
         this.handleAnimationEnd = this.handleAnimationEnd.bind(this);
+        this.getHourElement = this.getHourElement.bind(this);
+        this.getMinuteElement = this.getMinuteElement.bind(this);
+        this.getElement = this.getElement.bind(this);
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        if (nextProps.hour === nextState.hour && nextProps.minute === nextState.minute) {
+            return false;
+        }
     }
 
     handleAnimationEnd(e) {
-        console.log("finished");
+        if(e.target.classList.contains("hourAnimation")){
+            this.setState({
+                hourAnimating: false,
+                hour: this.props.hour
+            });
+        }
+        if(e.target.classList.contains("minuteAnimation")){
+            this.setState({
+                minuteAnimating: false,
+                minute: this.props.minute
+            });
+        }
+    }
+
+    // getDuration()
+    getHourElement(radius) {
+        const lengthPercent = 0.8;
+        return this.getElement(radius, 'hour', lengthPercent);
+    }
+
+    getMinuteElement(radius) {
+        const lengthPercent = 0.96;
+        return this.getElement(radius, 'minute', lengthPercent);
+    }
+
+    getElement(radius, HM, linePercent) {
+        const lineLength = radius*linePercent;
+        let element = <line x1="0" y1="0" x2="0" y2={-lineLength} transform="rotate({this.state[HM]} 0 0)" stroke="orange" stroke-width="5"/>;
+        if (this.state[HM] !== this.props[HM]) {
+            let rotate= keyframes`
+              0% {
+                transform: rotate(${this.state[HM]}deg);
+              }
+              100% {
+                transform: rotate(${this.props[HM]}deg);
+              }
+            `;
+
+            let Move= styled.g`
+              animation: ${rotate} 4s linear;
+              animation-fill-mode: forwards;
+            `;
+            let newState = {};
+            newState[HM + "Animating"] = true;
+            this.setState(newState);
+            element = (<Move className="{HM}Animation" onAnimationEnd={this.handleAnimationEnd}>
+                <line x1="0" y1="0" x2="0" y2={-lineLength} transform="rotate({this.state[HM]} 0 0)" stroke="orange" stroke-width="5"/>
+            </Move>); 
+        }
+        return element;
     }
 
     render() {
-        const viewBox = [window.innerWidth / -2, 100 - window.innerHeight, window.innerWidth, window.innerHeight];
         const radius = 100
+        const viewBox = [window.innerWidth / -2, 100 - window.innerHeight, window.innerWidth, window.innerHeight];
         const lengthMinute = radius*.96;
-        const lengthHour = radius*.8;
 
-        let rotate = keyframes`
-          0% {
-            transform: rotate(0deg);
-          }
-          100% {
-            transform: rotate(180deg);
-          }
-        `;
-
-        const Move = styled.g`
-          animation: ${rotate} 4s linear;
-          animation-fill-mode: forwards;
-        `;
+        let hour = this.getHourElement(radius);
+        let minute = this.getMinuteElement(radius);
 
         return (
             <svg
@@ -38,10 +91,8 @@ class Clock extends React.Component {
                 viewBox = {viewBox}
                 >
                 <circle cx={0} cy={0} r={radius}/>
-                <line x1="0" y1="0" x2="0" y2={-lengthMinute} transform="rotate(0 0 0)" stroke="orange" stroke-width="5"/>
-                <Move id="hy" onAnimationEnd={this.handleAnimationEnd}>
-                    <line x1="0" y1="0" x2="0" y2={-lengthHour} transform="rotate(90 0 0)" stroke="orange" stroke-width="5"/>
-                </Move>
+                {minute}
+                {hour}
             </svg>
         );
     }
